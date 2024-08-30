448a65a83b5229541c966efe0327a79dfade6a11
AlbaReverte <albareverteruiz@gmail.com>
2024-08-30 10:05:52 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -6,6 +6,19 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let nombre = ["El gato", "Mi madre", "El doctor", "Mi hermana"];
+  let acción = [
+    "se cayó",
+    "cogió una pelota",
+    "hizo la comida",
+    "rescató un perro"
+  ];
+  let lugar = ["en la playa", "en el bar", "en el parque", "en el hospital"];
+
+  let nombreIndex = Math.floor(Math.random() * nombre.length);
+  let acciónIndex = Math.floor(Math.random() * acción.length);
+  let lugarIndex = Math.floor(Math.random() * lugar.length);
+
+  document.querySelector("#excusa").innerHTML =
+    nombre[nombreIndex] + " " + acción[acciónIndex] + " " + lugar[lugarIndex];
 };
